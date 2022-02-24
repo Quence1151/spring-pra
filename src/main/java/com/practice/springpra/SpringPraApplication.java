9bd0279b85eb1064f3efc44e5bca8892c3a3ef3d
Quence1151 <dlgksdyd1151@naver.com>
2022-02-25 00:11:47 +0900

    feat test

---
+++ src/main/java/com/practice/springpra/SpringPraApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springpra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/springpra/SpringPraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springpra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
